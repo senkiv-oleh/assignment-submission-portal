fccaf08921ed7513015bb3b5a3cac88b9645e867
Oleh Senkiv <o.senkiv.v@gmail.com>
2025-04-03 20:18:47 +0300

    Some fixes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,7 +28,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <main className="flex items-center justify-center min-h-screen bg-gray-100 p-6">
+          {children}
+        </main>
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,5 @@
 import AssignmentForm from "@/components/AssignmentForm";
 
 export default function Home() {
-  return (
-    <main className="flex items-center justify-center min-h-screen bg-gray-100 p-6">
-      <AssignmentForm />
-    </main>
-  );
+  return <AssignmentForm />;
 }
